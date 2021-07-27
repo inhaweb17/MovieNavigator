f0bf07db5278a24855bd352c6f4a8098f83e1b64
liswktjs <60773373+liswktjs@users.noreply.github.com>
2021-07-27 22:48:11 +0900

    js작업들이 남음
    html과 css를 대충 마무리  했음

---
+++ js/login.js
@@ -0,0 +1,4 @@
+//아이디와 비밀번호 입력시 서버로 전송하는 곳 
+const okBtn = document.querySelector('.ok_btn');
+const idText = document.querySelector('.idContainer input');
+const pwText = document.querySelector('.pwContainer input');
